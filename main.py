5929901814c00dcb200cca1f498ceb24f32218ff
t1mar <timurskoblej@gmail.com>
2025-12-19 01:14:05 +0200

    Creating main of game

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import pyglet
 
+class GameWindow(pyglet.window.Window):
+    def __init__(self):
+        super().__init__(width=1000, height=800, caption="2.5D Pyglet Engine", resizable=True)
+        self.batch = pyglet.graphics.Batch()
+
+    def update(self, dt):
+        pass
+
+if __name__ == "__main__":
+    window = GameWindow()
+
+    pyglet.clock.schedule_interval(window.update, 1 / 60.0) #60 FPS
+
+    pyglet.app.run()
